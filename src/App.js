3d3689c574cf1c044909099fbebc0f73a136c27b
Shashi5657 <sangepushashidhar@gmail.com>
2024-01-27 23:00:44 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Loading from "./Loading";
+import Tours from "./Tours";
+
+const url = "https://course-api.com/react-tours-project";
 
 function App() {
+  const [tours, setTours] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  function removeTour(tourID) {
+    // add logic to update state for tour removal
+    const updatedTours = tours.filter((tour) => tour.id !== tourID);
+    setTours(updatedTours);
+  }
+
+  async function fetchTours() {
+    // set loading to true here
+    setLoading(true);
+
+    const data = await fetch(url);
+    const jsonData = await data.json();
+    setTours(jsonData);
+
+    setLoading(false);
+    // set loading to false here
+  }
+
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
+  // if (loading) {
+  // 	return (
+  // 		<main>
+  // 			<Loading />
+  // 		</main>
+  // 	);
+  // }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      {loading ? <Loading /> : <Tours tours={tours} removeTour={removeTour} />}
+    </main>
   );
 }
 
